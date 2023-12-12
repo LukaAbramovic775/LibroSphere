04522a3ed3dd1a21c635494c5e9c59e0acdfc61c
Team-4-prog <60031881+LukaAbramovic775@users.noreply.github.com>
2023-12-12 16:04:42 +0100

    Modeli za knjigu, korisnika, narudzbu

---
+++ models.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, EmailStr
+from typing import List
+
+# Model klase za Knjigu
+class Knjiga(BaseModel):
+    naziv: str
+    autor: str
+    zanr: str
+    cijena: float
+    isbn: str
+    godina_izdanja: int
+    broj_stranica: int
+    opis: str
+
+# Modelklase za Korisnika
+class KorisnikCreate(BaseModel):
+    email: EmailStr
+    lozinka: str
+
+class Korisnik(BaseModel):
+    email: EmailStr
+    id: str
+
+# Model klase za Narudžbu
+class Narudzba(BaseModel):
+    korisnik_id: str
+    knjige: List[str]  # Lista ID-ova knjiga
+    ukupna_cijena: float
