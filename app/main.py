2aa3880b50d7f449b83c26cda802fbb99b19042d
Luka Abramović <luka.abramovic1@gmail.com>
2024-07-11 12:33:15 +0200

    docker,nginx

--- app/main.py
+++ app/main.py
@@ -9,3 +9,11 @@ app.add_event_handler("shutdown", close_mongo_connection)
 
 app.include_router(router)
 
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
+
+app.add_event_handler("startup", connect_to_mongo)
+app.add_event_handler("shutdown", close_mongo_connection)
+
+app.include_router(router)
